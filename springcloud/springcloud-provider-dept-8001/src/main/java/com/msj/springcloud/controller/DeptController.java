8023728dca1d506c7d757f7e3ec2463df446c1bf
msj <2669159659@qq.com>
2021-08-07 15:17:50 +0800

    Eureka服务注册，信息配置

--- springcloud/springcloud-provider-dept-8001/src/main/java/com/msj/springcloud/controller/DeptController.java
+++ springcloud/springcloud-provider-dept-8001/src/main/java/com/msj/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.msj.springcloud.controller;
 import com.msj.springcloud.api.pojo.Dept;
 import com.msj.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,11 @@ import java.util.List;
 public class DeptController {
     @Autowired
     private DeptService deptService;
+
+    // 获得一些配置信息，得到具体的微服务
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @PostMapping("/dept/add")
     public boolean addDept(Dept dept) {
         return deptService.addDept(dept);
@@ -29,4 +36,20 @@ public class DeptController {
     public List<Dept> queryAll() {
         return deptService.queryAll();
     }
+
+    // 注册进来的微服务，我们可以获取一些消息，可以获取我们配置文件中配置的info
+    @GetMapping("/dept/discovery")
+    public Object discovery() {
+        // 获得微服务列表的清单
+        List<String> service = discoveryClient.getServices();
+        System.out.println("discover=>service:" + service);
+
+        // 得到一个具体的微服务信息:通过具体的微服务id，也就是applicationName
+        List<ServiceInstance> instances = discoveryClient.getInstances("springcloud-provider-dept");
+        for(ServiceInstance instance : instances) {
+            System.out.println(instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri() + "\t" + instance.getServiceId());
+        }
+        return this.discoveryClient;
+    }
+
 }
