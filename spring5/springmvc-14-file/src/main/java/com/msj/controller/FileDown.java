bf9dc4f40cbc0f35618167fc893dfaa609305d89
msj <2669159659@qq.com>
2021-08-10 20:52:53 +0800

    spring文件下载

---
+++ spring5/springmvc-14-file/src/main/java/com/msj/controller/FileDown.java
@@ -0,0 +1,47 @@
+package com.msj.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+
+@RestController
+public class FileDown {
+    @RequestMapping(value="/download")
+    public String downloads(HttpServletResponse response , HttpServletRequest request) throws Exception{
+        //要下载的图片地址
+        String  path = request.getRealPath("/upload");
+        String  fileName = "孟顺建课表.pdf";
+
+        //1、设置response 响应头
+        response.reset(); //设置页面不缓存,清空buffer
+        response.setCharacterEncoding("UTF-8"); //字符编码
+        response.setContentType("multipart/form-data"); //二进制传输数据
+        //设置响应头
+        response.setHeader("Content-Disposition",
+                "attachment;fileName="+ URLEncoder.encode(fileName, "UTF-8"));
+
+        File file = new File(path,fileName);
+        //2、 读取文件--输入流
+        InputStream input=new FileInputStream(file);
+        //3、 写出文件--输出流
+        OutputStream out = response.getOutputStream();
+
+        byte[] buff =new byte[1024];
+        int index=0;
+        //4、执行 写出操作
+        while((index= input.read(buff))!= -1){
+            out.write(buff, 0, index);
+            out.flush();
+        }
+        out.close();
+        input.close();
+        return "ok";
+    }
+}
