01c15410cea73d50af9b5baa57824311c61cc96c
msj <2669159659@qq.com>
2021-08-11 21:06:17 +0800

    springboot项目框架

---
+++ springBoot/spring-01-helloword/src/test/java/com/msj/Spring01HellowordApplicationTests.java
@@ -0,0 +1,15 @@
+package com.msj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring01HellowordApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
