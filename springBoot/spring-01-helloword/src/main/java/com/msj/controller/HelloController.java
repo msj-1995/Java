6c29bd30171103c1ab9a911f30dfccd77f5e7cc9
msj <2669159659@qq.com>
2021-08-11 21:13:03 +0800

    springboot web开发->controller类

---
+++ springBoot/spring-01-helloword/src/main/java/com/msj/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.msj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello SpringBoot";
+    }
+}
