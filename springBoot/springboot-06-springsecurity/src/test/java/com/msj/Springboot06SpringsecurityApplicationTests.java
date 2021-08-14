7613d3fc0b912064032f76b6c9e39d610208bf43
msj <2669159659@qq.com>
2021-08-14 23:31:43 +0800

    spring test

---
+++ springBoot/springboot-06-springsecurity/src/test/java/com/msj/Springboot06SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot06SpringsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
