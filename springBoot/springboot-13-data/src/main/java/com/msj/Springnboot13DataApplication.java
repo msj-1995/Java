1366f3d2b56cbe338b0dc50903d5e393e24d9139
msj <2669159659@qq.com>
2021-08-23 13:07:00 +0800

    主启动类

---
+++ springBoot/springboot-13-data/src/main/java/com/msj/Springnboot13DataApplication.java
@@ -0,0 +1,13 @@
+package com.msj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springnboot13DataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springnboot13DataApplication.class, args);
+    }
+
+}
