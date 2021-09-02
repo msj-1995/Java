81dd9968b3f1d6dc916a0e2e873d962d54163812
msj <2669159659@qq.com>
2021-09-02 20:05:56 +0800

    es主启动类

---
+++ springBoot/springboot-11-es/src/main/java/com/msj/MsjEsApiApplication.java
@@ -0,0 +1,13 @@
+package com.msj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsjEsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsjEsApiApplication.class, args);
+    }
+
+}
