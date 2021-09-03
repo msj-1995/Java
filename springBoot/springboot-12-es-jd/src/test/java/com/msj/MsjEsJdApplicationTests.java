d04ddce2014db43c063218c0dda48107db8d370e
msj <2669159659@qq.com>
2021-09-03 20:08:27 +0800

    仿京东搜索测试

---
+++ springBoot/springboot-12-es-jd/src/test/java/com/msj/MsjEsJdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsjEsJdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
