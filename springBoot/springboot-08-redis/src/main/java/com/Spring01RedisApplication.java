cc2403ed12f1506017f647da2f1f394bb492d8df
msj <2669159659@qq.com>
2021-09-12 21:40:51 +0800

    redis启动类

---
+++ springBoot/springboot-08-redis/src/main/java/com/Spring01RedisApplication.java
@@ -0,0 +1,13 @@
+package com.msj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring01RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring01RedisApplication.class, args);
+    }
+
+}
