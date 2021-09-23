fd95a8656c5f7ab123a6af03fa8a8a409d2feeef
msj <2669159659@qq.com>
2021-09-23 21:30:16 +0800

    shardingjdbc 主启动

---
+++ sharding-jdbc/src/main/java/com/ShardingJdbcApplication.java
@@ -0,0 +1,16 @@
+package com.msj;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@MapperScan("com.msj.mapper")
+public class ShardingJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingJdbcApplication.class, args);
+    }
+
+}
