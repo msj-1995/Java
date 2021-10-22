5667481b94047807924a0b07a35063bd1bcd0d5f
msj <2669159659@qq.com>
2021-10-22 14:58:29 +0800

    blog mapper

---
+++ springBoot/springboot-03-test/src/main/java/com/msj/blog/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.msj.blog.mapper;
+
+import com.msj.blog.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 孟顺建
+ * @since 2021-05-08
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
