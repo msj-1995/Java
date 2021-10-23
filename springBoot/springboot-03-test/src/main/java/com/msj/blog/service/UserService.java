59aa233a5f0dcbadd322f4789ef15fe5f49d6f3a
msj <2669159659@qq.com>
2021-10-23 22:02:21 +0800

    blog service层

---
+++ springBoot/springboot-03-test/src/main/java/com/msj/blog/service/UserService.java
@@ -0,0 +1,16 @@
+package com.msj.blog.service;
+
+import com.msj.blog.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 孟顺建
+ * @since 2021-05-08
+ */
+public interface UserService extends IService<User> {
+
+}
