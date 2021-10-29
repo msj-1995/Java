ddf979073afe55acfff927193b9608258fe8aee4
msj <2669159659@qq.com>
2021-10-29 18:20:03 +0800

    blog controller提交

---
+++ springBoot/springboot-03-test/src/main/java/com/msj/blog/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.msj.blog.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 孟顺建
+ * @since 2021-05-08
+ */
+@RestController
+@RequestMapping("/blog/user")
+public class UserController {
+
+}
+
